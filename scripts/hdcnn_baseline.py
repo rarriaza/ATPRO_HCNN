d67205abf3dd8b992824ce3aa053c27711d38145
Sergio Liberman Bronfman <sergiolib@gmail.com>
2019-11-02 17:53:59 +0100

    Added data processing and downloading code

--- scripts/hdcnn_baseline.py
+++ scripts/hdcnn_baseline.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import datasets
 import models
+import logging
 from datetime import datetime
 
 
@@ -25,28 +26,57 @@ def get_results_directory():
     return results_directory
 
 
+def get_data_directory():
+    data_dir = args.data_dir
+    os.makedirs(data_dir, exist_ok=True)
+    return data_dir
+
+
 def get_logs_directory():
     logs_dir = "./logs"
     os.makedirs(logs_dir, exist_ok=True)
     return logs_dir
 
 
-def get_data(dataset):
+def get_data(dataset, data_directory):
     if dataset == 'cifar100':
-        tr, val, te = datasets.get_cifar100()
+        logging.info('Getting CIFAR-100 dataset')
+        tr, val = datasets.get_cifar100(data_directory)
+        te = None
+        logging.debug(
+            f'Training set: x_dims={tr[0].shape}, y_dims={tr[1].shape}')
     return tr, val, te
 
 
 def main(args):
+    logging.basicConfig(level=args.log_level)
+
+    logging.info('Creating directories')
+
     logs_directory = get_logs_directory()
+    logging.debug(f'Logs directory: {logs_directory}')
+
     model_directory = get_model_directory()
+    logging.debug(f'Models directory: {model_directory}')
+
+    data_directory = get_data_directory(args)
+    logging.debug(f'Data directory: {data_directory}')
+
     results_directory = get_results_directory()
-    training_data, validation_data, testing_data = get_data(args.dataset)
+    logging.debug(f'Results directory: {results_directory}')
+
+    logging.info('Getting data')
+    training_data, validation_data, _ = get_data(args.dataset, data_directory)
+
+    logging.info('Building model')
     net = models.HDCNNBaseline(logs_directory, model_directory, args)
+
     if args.train:
+        logging.info('Entering training')
         net.train(training_data, validation_data)
     if args.test:
-        net.predict(testing_data, results_directory)
+        logging.info('Entering testing')
+        logging.error('Not yet implemented')  # TODO: implement
 
 
 def parse_arguments():
@@ -65,6 +95,12 @@ def parse_arguments():
     parser.add_argument('-d', '--dataset', help='Dataset to use',
                         type=str, default='cifar100',
                         choices=['cifar100'])
+    parser.add_argument('--data_dir', help='Where to store data on the local'
+                                           ' machine (defaults to ./data)',
+                        type=str)
+    parser.add_argument('-l', '--log_level', help='Logs level',
+                        type=str, default='INFO',
+                        choices=['WARNING', 'INFO', 'DEBUG', 'ERROR'])
     parser.add_argument('-r', '--results', help='Results directory',
                         type=str, default='')
 
@@ -73,4 +109,9 @@ def parse_arguments():
 
 if __name__ == '__main__':
     args = parse_arguments()
+
+    # TODO: remove
+    args.train = True
+    args.log_level = 'DEBUG'
+
     main(args)
