96240aa6c5b83d2b67f577050dee7b529531472f
Sergio Liberman Bronfman <sergiolib@gmail.com>
2019-11-22 23:02:00 +0100

    Save models works again

--- models/resnet_baseline.py
+++ models/resnet_baseline.py
@@ -32,9 +32,6 @@ class ResNetBaseline(plugins.ModelSaverPlugin):
 
         self.logs_directory = logs_directory
 
-        self.ckpt = None
-        self.manager = None
-
         current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 
         self.tbCallback_train = tf.keras.callbacks.TensorBoard(
@@ -48,10 +45,9 @@ class ResNetBaseline(plugins.ModelSaverPlugin):
             'batch_size': 64,
             'initial_epoch': 0,
             'step': 5,  # Save weights every this amount of epochs
-            'stop': 500,
-            'lr': 0.001,
+            'stop': 10,
+            'lr': 0.00003,
             'lr_decay': 1e-6,
-            'fine_tune_epochs': 10
         }
 
         self.prediction_params = {
@@ -59,7 +55,6 @@ class ResNetBaseline(plugins.ModelSaverPlugin):
         }
 
     def train(self, training_data, validation_data):
-        x_train, y_train = training_data
         x_val, y_val = validation_data
 
         p = self.training_params
@@ -67,20 +62,14 @@ class ResNetBaseline(plugins.ModelSaverPlugin):
         self.adam_coarse = tf.keras.optimizers.Adam(lr=p['lr'], decay=p['lr_decay'])
         self.loss_fun = tf.keras.losses.CategoricalCrossentropy()
 
-        self.ckpt = tf.train.Checkpoint(step=tf.Variable(1), optimizer=self.adam_coarse, net=self.full_classifier)
-        self.manager = tf.train.CheckpointManager(self.ckpt, self.logs_directory, max_to_keep=3)
-
         index = p['initial_epoch']
-
-        # Unfreeze complete ResNet
-        utils.unfreeze_layers(self.full_classifier.layers)
 
         self.full_classifier.compile(optimizer=self.adam_coarse,
                                      loss='categorical_crossentropy',
                                      metrics=['accuracy'])
 
-        logger.info('Training coarse stage')
-        while index < p["fine_tune_epochs"]:
+        # logger.info('Training coarse stage')
+        while index < p["stop"]:
             validation_data = tf.data.Dataset.from_tensor_slices((x_val, y_val)).batch(batch_size=p["batch_size"])
             training_data = shuffle_data(training_data)
             x_train, y_train = training_data
@@ -90,37 +79,8 @@ class ResNetBaseline(plugins.ModelSaverPlugin):
                                      epochs=index + p['step'],
                                      validation_data=validation_data,
                                      callbacks=[self.tbCallback_train])
-            self.ckpt.step.assign_add(p['step'])
-            self.manager.save()
             index += p['step']
-
-        # Freeze last layers of ResNet for tuning last layer
-        utils.freeze_layers(self.full_classifier.layers[:-2])
-
-        # Recompile model
-        self.adam_fine = tf.keras.optimizers.Adam(lr=0.001, decay=1e-6)
-        self.full_classifier.compile(optimizer=self.adam_fine,
-                                     loss='categorical_crossentropy',
-                                     metrics=['accuracy'])
-        # Main train
-        logger.info('Training fine stage')
-        self.ckpt = tf.train.Checkpoint(step=tf.Variable(1), optimizer=self.adam_fine, net=self.full_classifier)
-        while index < p['stop']:
-            validation_data = tf.data.Dataset.from_tensor_slices((x_val, y_val)).batch(batch_size=p["batch_size"])
-            training_data = shuffle_data(training_data)
-            x_train, y_train = training_data
-            self.full_classifier.fit(x_train, y_train,
-                                     batch_size=p['batch_size'],
-                                     initial_epoch=index,
-                                     epochs=index + p['step'],
-                                     validation_data=validation_data,
-                                     callbacks=[self.tbCallback_train])
-            self.ckpt.step.assign_add(p['step'])
-            self.manager.save()
-            index += p['step']
-
-        # Unfreeze ResNet
-        utils.unfreeze_layers(self.full_classifier.layers[:-2])
+            self.save_model(f"saved_models/resnet_baseline_{index}-epochs.h5", self.full_classifier)
 
     def predict_fine(self, testing_data, results_file):
         x_test, y_test = testing_data
@@ -136,25 +96,6 @@ class ResNetBaseline(plugins.ModelSaverPlugin):
         utils.write_results(results_file, results_dict=results_dict)
 
         return yh_s
-
-    def predict_coarse(self, testing_data, results_file, fine2coarse):
-        x_test, y_test = testing_data
-
-        p = self.prediction_params
-
-        yh_s = self.full_classifier.predict(x_test, batch_size=p['batch_size'])
-
-        single_classifier_error = utils.get_error(y_test, yh_s)
-        logger.info('Single Classifier Error: ' + str(single_classifier_error))
-
-        yh_c = np.dot(yh_s, fine2coarse)
-        y_test_c = np.dot(y_test, fine2coarse)
-        coarse_classifier_error = utils.get_error(y_test_c, yh_c)
-
-        logger.info('Single Classifier Error: ' + str(coarse_classifier_error))
-        results_dict = {'Single Classifier Error': single_classifier_error,
-                        'Coarse Classifier Error': coarse_classifier_error}
-        utils.write_results(results_file, results_dict=results_dict)
 
     def build_full_classifier(self):
 
