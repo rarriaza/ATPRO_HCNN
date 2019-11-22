43218d4d71adb138a245a4e89f0beebf3c392097
Sergio Liberman Bronfman <sergiolib@gmail.com>
2019-11-23 00:09:30 +0100

    Better performance

--- models/resnet_baseline.py
+++ models/resnet_baseline.py
@@ -37,14 +37,17 @@ class ResNetBaseline(plugins.ModelSaverPlugin):
             log_dir=self.logs_directory + '/' + current_time,
             update_freq='epoch')  # How often to write logs (default: once per epoch)
         self.early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5)
+        self.reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.2,
+                                                              patience=5, min_lr=0.0000001)
+        self.model_checkpoint = tf.keras.callbacks.ModelCheckpoint(filepath=model_directory + "resnet_baseline_{epoch:02d}-epochs.h5",
+                                                                   save_freq=90000)
 
         self.training_params = {
             'batch_size': 64,
             'initial_epoch': 0,
             'step': 5,  # Save weights every this amount of epochs
             'stop': 100,
-            'lr': 0.00001,
-            'lr_decay': 1e-6,
+            'lr': 0.00003,
         }
 
         self.prediction_params = {
@@ -56,7 +59,7 @@ class ResNetBaseline(plugins.ModelSaverPlugin):
 
         p = self.training_params
 
-        self.adam_coarse = tf.keras.optimizers.Adam(lr=p['lr'], decay=p['lr_decay'])
+        self.adam_coarse = tf.keras.optimizers.Adam(lr=p['lr'])
         self.loss_fun = tf.keras.losses.CategoricalCrossentropy()
 
         index = p['initial_epoch']
@@ -66,18 +69,15 @@ class ResNetBaseline(plugins.ModelSaverPlugin):
                                      metrics=['accuracy'])
 
         # logger.info('Training coarse stage')
-        while index < p["stop"]:
-            validation_data = tf.data.Dataset.from_tensor_slices((x_val, y_val)).batch(batch_size=p["batch_size"])
-            training_data = shuffle_data(training_data)
-            x_train, y_train = training_data
-            self.full_classifier.fit(x_train, y_train,
-                                     batch_size=p['batch_size'],
-                                     initial_epoch=index,
-                                     epochs=index + p['step'],
-                                     validation_data=validation_data,
-                                     callbacks=[self.tbCallback_train, self.early_stopping])
-            index += p['step']
-            self.save_model(f"saved_models/resnet_baseline_{index}-epochs.h5", self.full_classifier)
+        training_data = shuffle_data(training_data)
+        x_train, y_train = training_data
+        self.full_classifier.fit(x_train, y_train,
+                                 batch_size=p['batch_size'],
+                                 initial_epoch=index,
+                                 epochs=index + p['stop'],
+                                 validation_data=(x_val, y_val),
+                                 callbacks=[self.tbCallback_train, self.early_stopping,
+                                            self.reduce_lr, self.model_checkpoint])
 
     def predict_fine(self, testing_data, results_file):
         x_test, y_test = testing_data
@@ -104,29 +104,3 @@ class ResNetBaseline(plugins.ModelSaverPlugin):
             self.n_fine_categories, activation='softmax')(net)
         return tf.keras.models.Model(inputs=model.input, outputs=net)
 
-    @tf.function
-    def train_step_fine(self, x_train, y_train):
-        with tf.GradientTape() as tape:
-            tr_loss = self.loss_fun(self.full_classifier(x_train), y_train)
-        gradients = tape.gradient(tr_loss, self.full_classifier.trainable_variables)
-        self.adam_fine.apply_gradients(zip(gradients, self.full_classifier.trainable_variables))
-        return tr_loss
-
-    @tf.function
-    def train_step_coarse(self, x_train, y_train):
-        with tf.GradientTape() as tape:
-            tr_loss = self.loss_fun(self.full_classifier(x_train), y_train)
-        gradients = tape.gradient(tr_loss, self.full_classifier.trainable_variables)
-        self.adam_coarse.apply_gradients(zip(gradients, self.full_classifier.trainable_variables))
-        return tr_loss
-
-    def evaluate(self, validation_data, n):
-        val_acc = 0
-        val_loss = 0
-        for x, y in validation_data:
-            y_pred = self.full_classifier(x)
-            val_loss += self.loss_fun(y, y_pred) * len(y)
-            tmp = tf.equal(tf.argmax(y_pred, 1), tf.argmax(y, 1))
-            val_acc += tf.reduce_sum(tmp)
-        val_acc = val_acc / n
-        return val_loss, val_acc
