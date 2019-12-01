396ced7c9ef3f40f2bcd4738bf6d883f0856a929
Kostis-S-Z <kostis.s.z.24@gmail.com>
2019-12-01 19:51:06 +0100

    Update script

--- run_resnet_attention.sh
+++ run_resnet_attention.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 export PYTHONPATH=".:$PYTHONPATH"
-python ./scripts/resnet_attention.py -tr_c -tr_f -te
+python ./scripts/resnet_attention.py -tr_c
+python ./scripts/resnet_attention.py -tr_f
+python ./scripts/resnet_attention.py -te
+
