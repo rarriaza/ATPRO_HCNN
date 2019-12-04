3a39734480deb8da05212fcf4f4edd4d211ed46a
rarriaza <rarriazab@gmail.com>
2019-12-04 14:59:06 +0100

    saving results for baseline architecture

--- results/confusion_matrix.py
+++ results/confusion_matrix.py
@@ -2,7 +2,8 @@ import tensorflow as tf
 import json
 
 def load_json():
-    path = "/Volumes/Watermelon/results_pc/baseline_architecture/20191204-001227.json"
+    path = "/Volumes/Watermelon/results_pc/20191204-134841.json"
+    #20191204 - 134841.json
     with open(path) as json_file:
         data = json.load(json_file)
     return data
