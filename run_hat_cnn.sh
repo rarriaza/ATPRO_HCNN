9448bc8b96c2fc398c7815aeb69547850e32b13e
Kostis-S-Z <kostis.s.z.24@gmail.com>
2019-12-05 16:35:34 +0000

    Remove debug arg

--- run_hat_cnn.sh
+++ run_hat_cnn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export PYTHONPATH=".:$PYTHONPATH"
-python ./scripts/hat_cnn.py -debug -tr_c
-python ./scripts/hat_cnn.py -debug -tr_f
-python ./scripts/hat_cnn.py -debug -tr_full
-python ./scripts/hat_cnn.py -debug -te_full
+python ./scripts/hat_cnn.py -tr_c
+python ./scripts/hat_cnn.py -tr_f
+python ./scripts/hat_cnn.py -tr_full
+python ./scripts/hat_cnn.py -te_full
