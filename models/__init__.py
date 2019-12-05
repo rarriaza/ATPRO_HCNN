97daadd3b6c9684b37cba7347f29167278c17ba5
Kostis-S-Z <kostis.s.z.24@gmail.com>
2019-12-05 17:19:18 +0100

    Add hat cnn model

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,5 @@
-from .hdcnn_baseline import HDCNNBaseline
-from .resnet_attention import ResNetAttention
-from .resnet_baseline import ResNetBaseline
-from .baseline_architecture import BaselineArchitecture
+# from .hdcnn_baseline import HDCNNBaseline
+# from .resnet_attention import ResNetAttention
+# from .resnet_baseline import ResNetBaseline
+# from .baseline_architecture import BaselineArchitecture
+from .hat_cnn import HatCNN
